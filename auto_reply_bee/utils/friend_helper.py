e6aedb7a7a75f728dd59783486ff95b672adcc01
Liwin <li.ang-2@student.manchester.ac.uk>
2019-11-17 03:34:42 +0000

    Added text to speech feature

--- auto_reply_bee/utils/friend_helper.py
+++ auto_reply_bee/utils/friend_helper.py
@@ -5,6 +5,14 @@ Handle messages from friends
 import time
 import random
 import itchat
+# Import the required module for text
+# to speech conversion
+from gtts import gTTS
+
+# This module is imported so that we can
+# play the converted audio
+import os
+
 from auto_reply_bee.utils import config
 from auto_reply_bee.utils.data_collection import (
     get_bot_info
@@ -15,7 +23,6 @@ from auto_reply_bee.utils.common import (
 
 
 __all__ = ['handle_friend']
-
 
 def handle_friend(msg):
     try:
@@ -42,57 +49,98 @@ def handle_friend(msg):
         receive_text = msg.text  # Message content sent by friends.
         nick_name = FILEHELPER if uuid == FILEHELPER else msg.user.nickName       
         print('\n{} sent message：{}'.format(nick_name, receive_text))
+        words = receive_text.split()
 
-        # Future modification to fix possible bug
-        if (receive_text.lower() == "sing"):
-            reply_text = "send_music_command"
+        if (words[0].lower() == "/tts"):
+            if (len(words) == 1):
+                reply_text = "Please provide at least one word to convert to speech."
+                time.sleep(random.randint(1, 2))  # Sleep for one second.
 
-        elif (receive_text.lower() == "hi"):
-            reply_text = "send_hi_command"
+                prefix = conf.get('auto_reply_prefix', '')
+                if prefix:
+                    reply_text = '{}{}'.format(prefix, reply_text)
+
+                suffix = conf.get('auto_reply_suffix', '')
+                if suffix:
+                    reply_text = '{}{}'.format(reply_text, suffix)
+
+                itchat.send(reply_text, toUserName=uuid)
+                print('Replied by {}：{}'.format(nick_name, reply_text))
+            else:
+                (firstWord, rest) = receive_text.split(maxsplit=1)
+                itchat.send("You're in 'text to speech' mode. Converting your text.", toUserName=uuid)
+                mytext = rest
+                # Language in which you want to convert
+                language = 'en'
+
+                # Passing the text and language to the engine,
+                # here we have marked slow=False. Which tells
+                # the module that the converted audio should
+                # have a high speed
+                myobj = gTTS(text=mytext, lang=language, slow=False)
+
+                # Saving the converted audio in a mp3 file named
+                # welcome
+                myobj.save("textToSpeech.mp3")
+
+                time.sleep(random.randint(1, 2))
+                print('MP3 sending')
+                itchat.send("Please wait patiently.", toUserName=uuid)
+                reply = itchat.send_file('textToSpeech.mp3', toUserName=uuid)
+                print(reply['BaseResponse']['ErrMsg'])
+                print('MP3 sent')
+                
         else:
-            reply_text = get_bot_info(receive_text, uuid)  # Get auto reply
-        
-        if (reply_text == "send_music_command"):
-            time.sleep(random.randint(1, 2))
-            print('MP3 sending')
-            itchat.send("Singing now, please wait patiently.", toUserName=uuid)
-            reply = itchat.send_file('music.mp3', toUserName=uuid)
-            print(reply['BaseResponse']['ErrMsg'])
-            print('MP3 sent')
+            # Future modification to fix possible bug
+            if (words[0].lower() == "sing"):
+                reply_text = "send_music_command"
 
-        elif (reply_text == "send_hi_command"):
-            time.sleep(random.randint(1, 2))
-            print('Image sending')
-            reply_text = "Hi. Sending you cute bee gif."
+            elif (words[0].lower() == "hi"):
+                reply_text = "send_hi_command"
+            else:
+                reply_text = get_bot_info(receive_text, uuid)  # Get auto reply
+            
+            if (reply_text == "send_music_command"):
+                time.sleep(random.randint(1, 2))
+                print('MP3 sending')
+                itchat.send("Singing now, please wait patiently.", toUserName=uuid)
+                reply = itchat.send_file('music.mp3', toUserName=uuid)
+                print(reply['BaseResponse']['ErrMsg'])
+                print('MP3 sent')
 
-            prefix = conf.get('auto_reply_prefix', '')
-            if prefix:
-                reply_text = '{}{}'.format(prefix, reply_text)
+            elif (reply_text == "send_hi_command"):
+                time.sleep(random.randint(1, 2))
+                print('Image sending')
+                reply_text = "Hi. Sending you cute bee gif."
 
-            suffix = conf.get('auto_reply_suffix', '')
-            if suffix:
-                reply_text = '{}{}'.format(reply_text, suffix)
+                prefix = conf.get('auto_reply_prefix', '')
+                if prefix:
+                    reply_text = '{}{}'.format(prefix, reply_text)
 
-            itchat.send(reply_text, toUserName=uuid)
-            print('Replied by {}：{}'.format(nick_name, reply_text))
-            reply = itchat.send_image('bee.gif', toUserName=uuid)
-            print(reply['BaseResponse']['ErrMsg'])
-            print('Image sent') 
-        
-        elif reply_text:  # If content is not empty, reply.
-            time.sleep(random.randint(1, 2))  # Sleep for one second.
+                suffix = conf.get('auto_reply_suffix', '')
+                if suffix:
+                    reply_text = '{}{}'.format(reply_text, suffix)
 
-            prefix = conf.get('auto_reply_prefix', '')
-            if prefix:
-                reply_text = '{}{}'.format(prefix, reply_text)
+                itchat.send(reply_text, toUserName=uuid)
+                print('Replied by {}：{}'.format(nick_name, reply_text))
+                reply = itchat.send_image('bee.gif', toUserName=uuid)
+                print(reply['BaseResponse']['ErrMsg'])
+                print('Image sent') 
+            
+            elif reply_text:  # If content is not empty, reply.
+                time.sleep(random.randint(1, 2))  # Sleep for one second.
 
-            suffix = conf.get('auto_reply_suffix', '')
-            if suffix:
-                reply_text = '{}{}'.format(reply_text, suffix)
+                prefix = conf.get('auto_reply_prefix', '')
+                if prefix:
+                    reply_text = '{}{}'.format(prefix, reply_text)
 
-            itchat.send(reply_text, toUserName=uuid)
-            print('Replied by {}：{}'.format(nick_name, reply_text))
-        else:
-            print('Fail to auto reply\n')
+                suffix = conf.get('auto_reply_suffix', '')
+                if suffix:
+                    reply_text = '{}{}'.format(reply_text, suffix)
+
+                itchat.send(reply_text, toUserName=uuid)
+                print('Replied by {}：{}'.format(nick_name, reply_text))
+            else:
+                print('Fail to auto reply\n')
     except Exception as exception:
         print(str(exception))
